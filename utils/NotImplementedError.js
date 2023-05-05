b2140d51e8ac923e63424ab0dc89c521c0085a6c
mattino <mattino13@yandex.ru>
2023-05-05 21:49:15 +0300

    sprint14-pr2-hotfix1

--- utils/NotImplementedError.js
+++ utils/NotImplementedError.js
@@ -1,9 +1,9 @@
 const { HTTPError } = require('./HTTPError');
-const { COMMON_SERVER_ERROR_STATUS } = require('./consts');
+const { NOT_FOUND_HTTP_STATUS } = require('./consts');
 
 class NotImplementedError extends HTTPError {
   constructor(message) {
-    super(message, COMMON_SERVER_ERROR_STATUS);
+    super(message, NOT_FOUND_HTTP_STATUS);
   }
 }
 
